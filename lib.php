d9406bc44429086f98a6a222f9344d063c12d760
Uttapong Ruangrit <uttapongr@sanook.com>
2015-12-01 13:54:01 +0700

    update quota

--- lib.php
+++ lib.php
@@ -38,6 +38,13 @@ function getInfo($fbid){
 
 function getquota($rank){
   global $conn;
+  if($rank=='C'||$rank=='P')
+    $arr_result=['G'=>8,'B'=>8,'P'=>8,'Y'=>8];
+  else if($rank=='S')
+    $arr_result=['G'=>4,'B'=>4,'P'=>4,'Y'=>4];
+  else $arr_result=['G'=>10,'B'=>10,'P'=>10,'Y'=>10];
+
+
   $arr_result=['G'=>16,'B'=>16,'P'=>16,'Y'=>16];
   $result = $conn->query("select color,count(id) as remain from member where player=1 and rank='{$rank}' group by color");
   while($row = $result->fetch_assoc())
@@ -70,7 +77,7 @@ function random($rank,$fbid){
   {
     if($rank=='C'||$rank=='P')
       $arr_result[$row[color]]=8-$row[remain];
-    elseif($rank=='S')
+    else if($rank=='S')
       $arr_result[$row[color]]=4-$row[remain];
     else $arr_result[$row[color]]=10-$row[remain];
   }
